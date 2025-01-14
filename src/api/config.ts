74219d2ce7433f356177f4b8107f4af2db59ac29
IamSubarist <optimistroma927@gmail.com>
2025-01-14 13:07:13 +0300

    в api/config.ts => axiosInstance появился method и params. Благодаря этому в get-character.ts теперь await axiosInstance без .get

--- src/api/config.ts
+++ src/api/config.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const RICK_AND_MORTY_API_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
-  //   method: "GET",
+  method: "GET",
+  params: { pagination: 1 },
   baseURL: RICK_AND_MORTY_API_URL,
 });
--- src/api/get-character.ts
+++ src/api/get-character.ts
@@ -1,18 +1,9 @@
 import { CHARACTER_REQUEST_TARGET } from "./constans";
 import { axiosInstance } from "./config";
 
-// axiosInstance({
-//   method: 'GET',
-//   params: {
-//     pagination: '1'
-//   }
-// })
-// '..../api/characters?pagination=1'
-
 export const getCharacters = async () => {
   try {
-    const response = await axiosInstance.get(CHARACTER_REQUEST_TARGET);
-    // console.log(response.data);
+    const response = await axiosInstance(CHARACTER_REQUEST_TARGET);
     return response.data;
   } catch (error) {
     console.error(error);
